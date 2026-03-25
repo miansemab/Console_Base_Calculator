2cee7679ac2ce5027cc24c12f5a1e5079e61307c
Mian Semab <miansemab8073@gmail.com>
2026-03-25 12:42:18 +0500

    Console_base_Calculator

---
+++ main.py
@@ -0,0 +1,12 @@
+first_number=int(input("Enter first number: "))
+second_number=int(input("Enter second number: "))
+operation=input("Enter operation , + , _ , * , / , : ")
+if operation=="+":
+    print("The sum is",first_number+second_number)
+elif operation=="-":
+         print("The difference is",first_number-second_number)
+elif operation=="*":
+    print("The multiplication is",first_number*second_number)
+elif operation=="/":
+    print("The division is",first_number/second_number)
+
